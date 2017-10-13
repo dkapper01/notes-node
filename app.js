e0eb7dc25729036e259d87349111075cc67d562b
Daniel Kapper <dkapper01@gmail.com>
2017-10-13 13:53:18 -0400

    test atom

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const _ = require('lodash'); 
+const _ = require('lodash');
 
 const notes = require('./notes.js');
+const argv = yargs.argv; 
 
-var command = process.argv[2]; 
-console.log('Command ' + command); 
-console.log(process.argv); 
+var command = process.argv[2];
+console.log('Command ' + command);
+console.log('Process', process.argv);
+console.log('Yargs', argv);
+console.log(process.argv);
 
 if (command === 'add') {
-	console.log('adding new note'); 
+	console.log('adding new note');
 } else if (command === 'list') {
-	console.log('Listing all notes'); 
+	console.log('Listing all notes');
 } else if (command === 'read') {
 	console.log('Reading note')
 } else if (command === 'remove') {
-	console.log('Remove note'); 
+	console.log('Remove note');
 } else {
 	console.log('Command not recognized');
 }
-
-
